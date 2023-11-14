35acc3ad3a87358118abb06f0dae69067cb5b5a7
suiwombat <joe@mystenlabs.com>
2023-11-14 09:39:50 -0600

    buck targets and fixups

---
+++ third-party/gen_fixups.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Check if exactly one argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 folder name here"
+    exit 1
+fi
+
+folder="$1"
+
+mkdir fixups/$folder
+touch fixups/$folder/fixups.toml
